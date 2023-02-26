6674bd3f3360e3943e1c99f5b8074e485de721bf
Sofia Dzyuba <Sofia.Dzyuba@tptlive.ee>
2023-02-26 21:01:07 +0200

    6

---
+++ index.js
@@ -0,0 +1,16 @@
+var readlineSync = require('readline-sync');
+console.log("am i asking this question in spanish?")
+var answer = readlineSync.question('Answer: ');
+console.log(answer)
+
+console.log("How many days are in week?")
+var answer = readlineSync.question('Answer: ');
+console.log(answer)
+
+console.log("Dogs or cats?")
+var answer = readlineSync.question('Answer: ');
+console.log(answer)
+
+console.log("How many stars are in the sky?")
+var answer = readlineSync.question('Answer: ');
+console.log(answer)
